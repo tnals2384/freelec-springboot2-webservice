5ef2f1c5e9d8d540e59e6714270717d3dbb4c386
Sumin <tnals2384@gmail.com>
2022-07-26 17:24:44 +0900

    구글 로그인 연동

--- src/main/java/org/example/springboot/web/IndexController.java
+++ src/main/java/org/example/springboot/web/IndexController.java
@@ -22,7 +22,7 @@ public class IndexController {
         model.addAttribute("posts", postsService.findAllDesc());
         SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null) {
-            model.addAttribute("username", user.getName());
+            model.addAttribute("userName", user.getName());
         }
         return "index";
     }
@@ -39,6 +39,5 @@ public class IndexController {
 
         return "posts-update";
     }
-
 
 }
